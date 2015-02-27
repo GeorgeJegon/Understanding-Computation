03e26260a2cc2be27169d187543d8a077c5c8731
George Jegon <george_jegon@hotmail.com>
2015-02-27 17:28:51 -0300

    Created if and lessthan class verifications

---
+++ lessthan.rb
@@ -0,0 +1,28 @@
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if left.reducible?
+      LessThan.new(left.reduce(environment), right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce(environment))
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+
+  def evaluate(environment)
+    Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+end
+
